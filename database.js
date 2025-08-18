4293b70ad93a81deb5b4608b1d6072ee59f0efd3
Lucas Rodrigues <santoslucasrd@outlook.com>
2025-08-18 00:22:57 -0300

    Readicionando o database

---
+++ database.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const mongoose = require('mongoose');
+
+const MONGODB_URI = `mongodb+srv://${dbUser}:${dbPassword}@clustermini.8o9abxk.mongodb.net/?retryWrites=true&w=majority&appName=Clustermini`;
+
+async function connectDB() {
+    try {
+         await mongoose.connect(MONGODB_URI);
+         console.log('Conectado ao MongoDB.');
+    } catch (error) {
+        console.error('Erro ao conectar ao MongoDB:', error);
+    }
+}
+
+module.exports = { connectDB };
